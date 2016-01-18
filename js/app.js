a7c5e32ed980453c5e41be7a6ff9017cdb8113e3
mahesh maurya <mahesh@wohlig.com>
2016-01-18 15:26:45 +0530

    git for djtush

--- js/app.js
+++ js/app.js
@@ -1,94 +1,113 @@
 // JavaScript Document
 var firstapp = angular.module('firstapp', [
-    'ui.router',
-    'phonecatControllers',
-    'templateservicemod',
-    'navigationservice'
+  'ui.router',
+  'phonecatControllers',
+  'templateservicemod',
+  'navigationservice'
 ]);
 
-firstapp.config(function($stateProvider, $urlRouterProvider,$httpProvider) {
+firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
 
-    // for http request with session
-    $httpProvider.defaults.withCredentials = true;
+  // for http request with session
+  $httpProvider.defaults.withCredentials = true;
 
-    $stateProvider
+  $stateProvider
 
     .state('home', {
-        url: "/home",
-        templateUrl: "views/template.html",
-        controller: 'HomeCtrl'
+      url: "/home",
+      templateUrl: "views/template.html",
+      controller: 'HomeCtrl'
     })
     .state('players', {
-        url: "/players",
-        templateUrl: "views/template.html",
-        controller: 'PlayersCtrl'
+      url: "/players",
+      templateUrl: "views/template.html",
+      controller: 'PlayersCtrl'
     })
     .state('wallpapper', {
-        url: "/wallpapper",
-        templateUrl: "views/template.html",
-        controller: 'WallpapperCtrl'
+      url: "/wallpapper",
+      templateUrl: "views/template.html",
+      controller: 'WallpapperCtrl'
     })
     .state('wallpapper-inner', {
-        url: "/wallpapper-inner",
-        templateUrl: "views/template.html",
-        controller: 'WallpapperInnerCtrl'
+      url: "/wallpapper-inner",
+      templateUrl: "views/template.html",
+      controller: 'WallpapperInnerCtrl'
     })
     .state('gallery', {
-        url: "/gallery",
-        templateUrl: "views/template.html",
-        controller: 'GalleryCtrl'
+      url: "/gallery",
+      templateUrl: "views/template.html",
+      controller: 'GalleryCtrl'
     })
     .state('fixtures', {
-        url: "/fixtures",
-        templateUrl: "views/template.html",
-        controller: 'FixturesCtrl'
+      url: "/fixtures",
+      templateUrl: "views/template.html",
+      controller: 'FixturesCtrl'
     })
     .state('news-media', {
-        url: "/news-media",
-        templateUrl: "views/template.html",
-        controller: 'NewsMediaCtrl'
+      url: "/news-media",
+      templateUrl: "views/template.html",
+      controller: 'NewsMediaCtrl'
     })
     .state('news-detail', {
-        url: "/news-detail",
-        templateUrl: "views/template.html",
-        controller: 'NewsDetailCtrl'
+      url: "/news-detail",
+      templateUrl: "views/template.html",
+      controller: 'NewsDetailCtrl'
     })
     .state('ticket-merchandize', {
-        url: "/ticket-merchandize",
-        templateUrl: "views/template.html",
-        controller: 'TicketMerchandizeCtrl'
+      url: "/ticket-merchandize",
+      templateUrl: "views/template.html",
+      controller: 'TicketMerchandizeCtrl'
     })
 
-    .state('about-us', {
-        url: "/about-us",
-        templateUrl: "views/template.html",
-        controller: 'AboutUsCtrl'
-    });
+  .state('about-us', {
+    url: "/about-us",
+    templateUrl: "views/template.html",
+    controller: 'AboutUsCtrl'
+  });
 
-    $urlRouterProvider.otherwise("/home");
+  $urlRouterProvider.otherwise("/home");
 
 });
 
+firstapp.directive('fancyboxThumb', function() {
+  return {
+    restrict: 'C',
+    replace: false,
+    link: function($scope, element, attrs) {
+      $('.fancybox-thumb').fancybox({
+        prevEffect: 'none',
+        nextEffect: 'none',
+        helpers: {
+          title: {
+            type: 'outside'
+          },
+          thumbs: {
+            width: 50,
+            height: 50
+          }
+        }
+      });
+
+    }
+  };
+});
 
 firstapp.directive('img', function($compile, $parse) {
-    return {
-        restrict: 'E',
-        replace: false,
-        link: function($scope, element, attrs) {
-            var $element = $(element);
-            if(!attrs.noloading)
-            {
-                $element.after("<img src='img/loading.gif' class='loading' />");
-                var $loading = $element.next(".loading");
-                $element.load(function() {
-                    $loading.remove();
-                    $(this).addClass("doneLoading");
-                });
-            }
-            else
-            {
-                $($element).addClass("doneLoading");
-            }
-        }
-    };
+  return {
+    restrict: 'E',
+    replace: false,
+    link: function($scope, element, attrs) {
+      var $element = $(element);
+      if (!attrs.noloading) {
+        $element.after("<img src='img/loading.gif' class='loading' />");
+        var $loading = $element.next(".loading");
+        $element.load(function() {
+          $loading.remove();
+          $(this).addClass("doneLoading");
+        });
+      } else {
+        $($element).addClass("doneLoading");
+      }
+    }
+  };
 });
