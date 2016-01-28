ec7f9f8e708436a795209d5876b1cde96f6eee40
Rohan Gada <gadarohan17@gmail.com>
2016-01-28 16:54:54 +0530

    newsmedia

--- js/app.js
+++ js/app.js
@@ -161,7 +161,24 @@ firstapp.directive('childHeight', function($compile, $parse) {
     }
   };
 });
+firstapp.filter('cut', function() {
+    return function(value, wordwise, max, tail) {
+        if (!value) return '';
 
+        max = parseInt(max, 10);
+        if (!max) return value;
+        if (value.length <= max) return value;
+
+        value = value.substr(0, max);
+        if (wordwise) {
+            var lastspace = value.lastIndexOf(' ');
+            if (lastspace != -1) {
+                value = value.substr(0, lastspace);
+            }
+        }
+        return value + (tail || ' …');
+    };
+});
 firstapp.directive("scroll", function($window) {
   return function(scope, element, attrs) {
     angular.element($window).bind("scroll", function() {
