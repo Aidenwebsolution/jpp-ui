03838f3a05c629108145d1a95bf735637e9f7641
mahesh maurya <mahesh@wohlig.com>
2016-01-28 12:05:36 +0530

    chnaes

--- js/controllers.js
+++ js/controllers.js
@@ -683,11 +683,13 @@ title:'LOREM IPSUM IS SIMPLY DUMMY TEXT'
     desp: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   }];
 
-  $scope.openPlayers = function() {
+  $scope.openPlayers = function(index) {
+    $scope.slideindex = index;
     $uibModal.open({
       animation: true,
       templateUrl: 'views/modal/player-slider.html',
-      controller: 'PlayersCtrl'
+      controller: 'PlayersCtrl',
+      scope:$scope
     })
   };
 })
