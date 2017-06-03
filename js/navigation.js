d5d36b546fd8d93cf1456b5db81cf8906d8c5e7d
NileshWangad <ruchira.padave29@gmail.com>
2017-06-03 12:02:03 +0530

    url chnaged

--- js/navigation.js
+++ js/navigation.js
@@ -1,59 +1,60 @@
-var mainurl = "http://admin.jaipurpinkpanthers.com/index.php/";
-// var mainurl = "http://192.168.0.101/jppbackend/index.php/";
-//wohlig url for testing
+var mainurl = "http://wohlig.co.in/pinkpanther/jppbackend/index.php"
+    // var mainurl = "http://admin.jaipurpinkpanthers.com/index.php/";
+    // var mainurl = "http://192.168.0.101/jppbackend/index.php/";
+    //wohlig url for testing
 
 var imgurl = "http://admin.jaipurpinkpanthers.com/uploads/";
-var tempUrl ="http://wohlig.co.in/jppbackend/index.php/json/";
+var tempUrl = "http://wohlig.co.in/jppbackend/index.php/json/";
 var tempimgurl = "http://wohlig.co.in/jppbackend/uploads/";
 // var imgurl = "http://192.168.1.107/jppbackend/uploads/";
 // var imgurl="";
 var adminurl = mainurl + "json/";
 var navigationservice = angular.module('navigationservice', [])
 
-.factory('NavigationService', function($http) {
+.factory('NavigationService', function ($http) {
     var navigation = [{
             name: "Fixtures & Results",
             // url: "fixtures({id:1})",
-               url: "Comingsoon",
+            url: "Comingsoon",
             classis: "active"
         }, {
             name: "Panther World",
             // url: "fixtures({id:1})",
-            id:"panther",
+            id: "panther",
             classis: "active",
             subnav: []
-            // {
-            //     name: "Wallpapers",
-            //     url: "wallpaper",
-            //     classis: "active"
-            // }, {
-            //     name: "Gallery",
-            //     url: "gallery",
-            //     classis: "active"
-            // }, {
-            //     name: "Games",
-            //     link: "jpp-tv",
-            //     classis: "active"
-            // }, {
-            //     name: "JPP TV",
-            //     url: "jpp-tv",
-            //     classis: "active"
-            // }
+                // {
+                //     name: "Wallpapers",
+                //     url: "wallpaper",
+                //     classis: "active"
+                // }, {
+                //     name: "Gallery",
+                //     url: "gallery",
+                //     classis: "active"
+                // }, {
+                //     name: "Games",
+                //     link: "jpp-tv",
+                //     classis: "active"
+                // }, {
+                //     name: "JPP TV",
+                //     url: "jpp-tv",
+                //     classis: "active"
+                // }
         }, {
             name: "Tickets",
             // url: "ticket",
-                url: "Comingsoon",
+            url: "Comingsoon",
             classis: "active"
         }, {
             name: "Merchandise",
             // url: "ticket-merchandise",
-                url: "Comingsoon",
+            url: "Comingsoon",
             classis: "active",
             highlighter: "high"
         }, {
             name: "Players",
             url: "players",
-                // url: "Comingsoon",
+            // url: "Comingsoon",
             classis: "active"
         },
         // {
@@ -87,131 +88,130 @@ var navigationservice = angular.module('navigationservice', [])
     ];
 
     return {
-        getnav: function() {
+        getnav: function () {
             return navigation;
         },
 
-        getAllSliders: function(callback) {
+        getAllSliders: function (callback) {
             $http.get(adminurl + "getAllSliders").then(callback);
         },
-        contactus: function(register, callback) {
+        contactus: function (register, callback) {
             $http({
                 url: adminurl + 'contactus',
                 method: 'POST',
                 data: register
             }).success(callback);
         },
-        submitFriends: function(frienddetail, callback) {
+        submitFriends: function (frienddetail, callback) {
             $http({
                 url: adminurl + 'submitClan',
                 method: 'POST',
                 data: frienddetail
             }).success(callback);
         },
-        getallpoint: function(callback) {
+        getallpoint: function (callback) {
             $http.get(adminurl + "getallpoint").then(callback);
         },
-        getLatestMatch: function(callback) {
+        getLatestMatch: function (callback) {
             $http.get(adminurl + "getLatestMatch").then(callback);
         },
-        getScheduleSeason4: function(callback) {
+        getScheduleSeason4: function (callback) {
             $http.get(adminurl + "getScheduleSeason4").then(callback);
         },
-        getHomeGameSeason4: function(callback) {
+        getHomeGameSeason4: function (callback) {
             $http.get(adminurl + "getHomeGameSeason4").then(callback);
         },
-        getAllGallery: function(callback) {
+        getAllGallery: function (callback) {
             $http.get(adminurl + "getAllGallery").then(callback);
         },
-        getAllVideoGallery: function(callback) {
+        getAllVideoGallery: function (callback) {
             $http.get(adminurl + "getAllVideoGallery").then(callback);
         },
-        getallnews: function(callback) {
+        getallnews: function (callback) {
             $http.get(adminurl + "getallnews").then(callback);
         },
-        getWallpaperCategoryForDesktop: function(callback) {
+        getWallpaperCategoryForDesktop: function (callback) {
             $http.get(adminurl + "getWallpaperCategoryForDesktop").then(callback);
         },
-        getSchedule: function(callback) {
+        getSchedule: function (callback) {
             $http.get(adminurl + "getSchedule").then(callback);
         },
-        getGallerySlide: function(request, callback) {
+        getGallerySlide: function (request, callback) {
             $http.get(adminurl + "getGallerySlide?galleryid=" + request).then(callback);
         },
 
-        getsingleplayer: function(id, callback) {
+        getsingleplayer: function (id, callback) {
             $http.get(tempUrl + "getsingleplayer?id=" + id).then(callback);
         },
 
 
-        getallwallpaper: function(request, callback) {
+        getallwallpaper: function (request, callback) {
             $http.get(adminurl + "getallwallpaper?wallpapercategory=" + request.wallpaperid + "&pageno=" + request.pageno).success(callback);
         },
-        getSlider: function(callback) {
+        getSlider: function (callback) {
             $http.get(adminurl + "getAllSliders").then(callback);
         },
 
-         getguesswho: function(callback) {
+        getguesswho: function (callback) {
             $http.get(adminurl + "getguesswho").success(callback);
         },
-        getpantherworldguesswho: function(callback) {
+        getpantherworldguesswho: function (callback) {
             $http.get(tempUrl + "getpantherworldguesswho").then(callback);
         },
 
-         getAuthenticate: function(callback) {
+        getAuthenticate: function (callback) {
             $http.get(adminurl + "authenticate").success(callback);
         },
-         logoutUser: function(callback) {
+        logoutUser: function (callback) {
             $http.get(adminurl + "logout").success(callback);
         },
-        submitSignup: function(formData, callback) {
-          $http({
-            url: adminurl + 'signup',
-            method: 'POST',
-            withCredentials: true,
-            data: formData
-          }).success(callback);
+        submitSignup: function (formData, callback) {
+            $http({
+                url: adminurl + 'signup',
+                method: 'POST',
+                withCredentials: true,
+                data: formData
+            }).success(callback);
         },
-        submitLogin: function(loginData, callback) {
-          $http({
-            url: adminurl + 'login',
-            method: 'POST',
-            withCredentials: true,
-            data: loginData
-          }).success(callback);
+        submitLogin: function (loginData, callback) {
+            $http({
+                url: adminurl + 'login',
+                method: 'POST',
+                withCredentials: true,
+                data: loginData
+            }).success(callback);
         },
-        getallplayers: function(callback) {
+        getallplayers: function (callback) {
             $http.get(tempUrl + "getallplayers").then(callback);
         },
 
-       getcongratulation: function(callback) {
-          $http.get(tempUrl + "getcongratulation").success(callback);
-      },
+        getcongratulation: function (callback) {
+            $http.get(tempUrl + "getcongratulation").success(callback);
+        },
 
-      getjourney: function(callback) {
-         $http.get(tempUrl + "getjourney").success(callback);
-     },
-       //  getjourney: function(callback) {
-       //     $http.get(adminurl + "getjourney").success(callback);
-       // },
-       changeTimerRapid: function() {
-         var rapidTimer = $.jStorage.get("rapidTimer");
-         var returnVal;
-         if(rapidTimer && rapidTimer != 1) {
-           returnVal  = rapidTimer - 1;
-           $.jStorage.set("rapidTimer",returnVal);
-         }
-         else if( rapidTimer != 1) {
-           $.jStorage.set("rapidTimer",90);
-           returnVal = 90;
-         } else {
-           $.jStorage.set("rapidTimer",null);
-           returnVal = 0;
-         }
-         return returnVal;
-       },
+        getjourney: function (callback) {
+            $http.get(tempUrl + "getjourney").success(callback);
+        },
+        //  getjourney: function(callback) {
+        //     $http.get(adminurl + "getjourney").success(callback);
+        // },
+        changeTimerRapid: function () {
+            var rapidTimer = $.jStorage.get("rapidTimer");
+            var returnVal;
+            if (rapidTimer && rapidTimer != 1) {
+                returnVal = rapidTimer - 1;
+                $.jStorage.set("rapidTimer", returnVal);
+            } else if (rapidTimer != 1) {
+                $.jStorage.set("rapidTimer", 90);
+                returnVal = 90;
+            } else {
+                $.jStorage.set("rapidTimer", null);
+                returnVal = 0;
+            }
+            return returnVal;
+        },
 
-        makeactive: function(menuname) {
+        makeactive: function (menuname) {
             for (var i = 0; i < navigation.length; i++) {
                 if (navigation[i].name == menuname) {
                     navigation[i].classis = "active";
