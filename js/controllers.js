22cf11163f669e8cd71b08f95a37b0c22028b683
sohan1 <shn619@gmail.com>
2017-07-25 19:53:59 +0530

    controler

--- js/controllers.js
+++ js/controllers.js
@@ -127,19 +127,19 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
                     console.log("userFirstName", $rootScope.userFirstName)
                     $rootScope.loggedIn = true;
                     if (value == 'Game') {
-                        window.location = "http://jaipurpinkpanthers.com/beta/pantherworld/#/games";
+                        window.location = "http://jaipurpinkpanthers.com/pantherworld/games";
                         // console.log("im in game");
                         // $state.go('Comingsoon');
                     }
 
                     if (value == 'JPP') {
-                        window.location = "http://jaipurpinkpanthers.com/beta/#/jpp-tv";
+                        window.location = "http://jaipurpinkpanthers.com/jpp-tv";
                     }
                     if (value == 'Gallery') {
-                        window.location = "http://jaipurpinkpanthers.com/beta/#/gallery";
+                        window.location = "http://jaipurpinkpanthers.com/gallery";
                     }
                     if (value == 'WALLPAPERS') {
-                        window.location = "http://jaipurpinkpanthers.com/beta/#/wallpaper";
+                        window.location = "http://jaipurpinkpanthers.com/wallpaper";
                     }
                 } else {
                     $rootScope.loggedIn = false;
@@ -2392,7 +2392,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
                     $rootScope.userFirstName = data.firstname;
                     console.log("userFirstName", $rootScope.userFirstName)
                     $rootScope.loggedIn = true;
-                    window.location = "http://jaipurpinkpanthers.com/beta/#/gallery";
+                    window.location = "http://jaipurpinkpanthers.com/gallery";
                 } else {
                     $rootScope.loggedIn = false;
                     $scope.modalLogsInstance = $uibModal.open({
@@ -2412,7 +2412,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
                     $rootScope.userFirstName = data.firstname;
                     console.log("userFirstName", $rootScope.userFirstName)
                     $rootScope.loggedIn = true;
-                    window.location = "http://jaipurpinkpanthers.com/beta/#/wallpaper";
+                    window.location = "http://jaipurpinkpanthers.com/wallpaper";
                 } else {
                     $rootScope.loggedIn = false;
                     $scope.modalLogsInstance = $uibModal.open({
@@ -2431,7 +2431,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
                     $rootScope.userFirstName = data.firstname;
                     console.log("userFirstName", $rootScope.userFirstName)
                     $rootScope.loggedIn = true;
-                    window.location = "http://jaipurpinkpanthers.com/beta/#/jpp-tv";
+                    window.location = "http://jaipurpinkpanthers.com/jpp-tv";
                 } else {
                     $rootScope.loggedIn = false;
                     $scope.modalLogsInstance = $uibModal.open({
